60a0243c593ee4054eedefffb77e4c6427cfe783
ayarmkt <ayat.0906@gmail.com>
2021-09-03 14:19:06 +0900

    updated project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,41 +1,57 @@
 import classes from './Projects.module.css';
 import Card from '../../UI/Card/Card';
 import H2 from '../../UI/H2/H2';
+import project3 from '../../assets/calculator.png';
 
 const projectArr = [
   {
     title: 'PROJECT TITLE1',
-    description: 'DESCRIPTION',
-
+    techName: ['React', 'Redux', 'Firebase'],
     src: 'https://blog.tubikstudio.com/wp-content/uploads/2021/04/1_tubik_Uplyfe_starting-300x180.jpg',
-    alt: 'dummy',
+    alt: 'bug tracker app',
+    description:
+      'A CRUD bug tracker app. The login is managed with React Context, and the app data is managed with Redux. Firebase is used for data storage.',
   },
   {
     title: 'PROJECT TITLE2',
-    description: 'DESCRIPTION',
-
+    techName: ['React', 'OpenWeatherMap API'],
     src: 'https://blog.tubikstudio.com/wp-content/uploads/2021/04/1_tubik_Uplyfe_starting-300x180.jpg',
-    alt: 'dummy',
+    alt: 'weather app',
+    description:
+      'A weather app with a minimalist design. You can search for the current weather conditions around the world. The background changes according to the weather.',
   },
   {
-    title: 'PROJECT TITLE3',
-    description: 'DESCRIPTION',
-
-    src: 'https://blog.tubikstudio.com/wp-content/uploads/2021/04/1_tubik_Uplyfe_starting-300x180.jpg',
-    alt: 'dummy',
+    title: 'CALCULATOR',
+    techName: ['HTML', 'CSS', 'JavaScript'],
+    src: project3,
+    alt: 'calculator',
+    description:
+      'A calculator made with JavaScript. It shows not only the result but also the process of calculation. It also supports +/- and % calculation.',
   },
 ];
 
 const Projects = () => {
   const projectList = projectArr.map((project) => {
+    const tech = project.techName.map((techName) => (
+      <p className={classes.techItem}>{techName}</p>
+    ));
+
     return (
       <div className={classes.project} key={project.title}>
-        <a href='' className={classes['project-img']}>
-          <img src={project.src} alt={project.alt} />
-        </a>
-        <div className={classes['project-text']}>
-          <h3>{project.title}</h3>
-          <p>{project.description}</p>
+        <div className={classes['project-img']}>
+          <a href=''>
+            <img src={project.src} alt={project.alt} />
+          </a>
+        </div>
+        <div className={classes['project-description']}>
+          <h3 className={classes.title}>{project.title}</h3>
+          <div className={classes.tech}>{tech}</div>
+
+          <p className={classes.description}>{project.description}</p>
+          <div className={classes.link}>
+            <a className={classes.live}>Live Demo</a>
+            <a className={classes.github}>GitHub</a>
+          </div>
         </div>
       </div>
     );
