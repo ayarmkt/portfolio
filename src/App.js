459a63f359fd11d4913dea3460315fd6c452fc8e
ayarmkt <ayat.0906@gmail.com>
2021-09-09 19:26:41 +0900

    link2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import './App.css';
 import React from 'react';
 //import { Route } from 'react-router-dom';
 //import classes from './App.css';
+import { useEffect } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 
 import Navbar from './components/Navbar/Navbar';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
+import { AiOutlineToTop } from 'react-icons/ai';
 
 function App() {
+  const scrollToTop = () => {
+    scroll.scrollToTop({ duration: 100 });
+  };
+
+  // let yOffset;
+  // let yScroll;
+  // useEffect(() => {
+  //   yOffset = window.pageYOffset;
+  //   console.log(yOffset);
+  //   yScroll = window.scrollY;
+  //   console.log(yScroll);
+  // }, [yOffset, yScroll]);
+
+  //let yScroll = window.scrollY;
+  //const visible = yScroll >= 300 ? visible : '';
+
   return (
     <React.Fragment>
       <header>
@@ -23,6 +42,11 @@ function App() {
         <Skills />
         <Projects />
         <Contact />
+        <AiOutlineToTop
+          className='to-top-icon'
+          size='40px'
+          onClick={scrollToTop}
+        />
       </main>
     </React.Fragment>
   );
