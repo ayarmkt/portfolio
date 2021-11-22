e523140f500c518bc077e03d6cf428e615fd4ccc
ayarmkt <ayat.0906@gmail.com>
2021-11-22 17:04:05 +0100

    added scroll to top icon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './App.scss';
 import React from 'react';
-import { useState, useEffect, useLayoutEffect, useContext } from 'react';
-import { animateScroll as scroll } from 'react-scroll';
+import {
+  //useState,
+  useEffect,
+  //useLayoutEffect,
+  useContext,
+} from 'react';
 import Hero from './components/Hero';
 import Navbar from './components/NavBar';
 import Projects from './components/Projects';
-
 import useWindowDimensions from './hooks/useWindowDimensions';
 import UIContext from './context/ui-context';
-import { AiOutlineToTop } from 'react-icons/ai';
 import AboutMe from './components/AboutMe';
 import Footer from './components/Footer';
 import HamburgerMenu from './components/HamburgerMenu';
 import Contacts from './components/Contacts';
+import ScrollToTopIcon from './UIcomponents/ScrollToTopIcon';
 
 function App() {
   const uiCtx = useContext(UIContext);
-  const [showIcon, setShowIcon] = useState(false);
+  // const [showIcon, setShowIcon] = useState(false);
   const { width: vw } = useWindowDimensions();
 
   useEffect(() => {
@@ -27,22 +30,18 @@ function App() {
     }
   }, [vw, uiCtx]);
 
-  const onScroll = () => {
-    if (80 < window.scrollY) {
-      setShowIcon(true);
-    } else {
-      setShowIcon(false);
-    }
-  };
+  // const onScroll = () => {
+  //   if (80 < window.scrollY) {
+  //     setShowIcon(true);
+  //   } else {
+  //     setShowIcon(false);
+  //   }
+  // };
 
-  useLayoutEffect(() => {
-    window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener('scroll', onScroll);
-  }, []);
-
-  const scrollToTop = () => {
-    scroll.scrollToTop({ duration: 700 });
-  };
+  // useLayoutEffect(() => {
+  //   window.addEventListener('scroll', onScroll);
+  //   return () => window.removeEventListener('scroll', onScroll);
+  // }, []);
 
   return (
     <React.Fragment>
@@ -58,15 +57,11 @@ function App() {
         {/* <About />
         <Skills />
         <Projects />*/}
-        <AiOutlineToTop
-          className={`to-top-icon ${showIcon ? 'showIcon' : ''}`}
-          size='50px'
-          onClick={scrollToTop}
-        />
       </main>
       {/* <Contacts /> */}
       <Footer />
       <Contacts />
+      <ScrollToTopIcon />
     </React.Fragment>
   );
 }
