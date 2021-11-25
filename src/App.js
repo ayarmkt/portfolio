4713e0f70066a2d75e60bda0900ed3c37dd03aba
ayarmkt <ayat.0906@gmail.com>
2021-11-25 09:52:30 +0100

    reset

--- src/App.js
+++ src/App.js
@@ -1,67 +1,8 @@
 import './App.css';
 import React from 'react';
-import { useState, useEffect, useLayoutEffect, useContext } from 'react';
-import { animateScroll as scroll } from 'react-scroll';
-
-import Navbar from './components/Navbar/Navbar';
-import About from './components/About/About';
-import Skills from './components/Skills/Skills';
-import Projects from './components/Projects/Projects';
-import HamburgerMenu from './components/Navbar/HamburgerMenu';
-import useWindowDimensions from './hooks/useWindowDimensions';
-import UIContext from './context/ui-context';
-import { AiOutlineToTop } from 'react-icons/ai';
-import Footer from './components/Footer/Footer';
 
 function App() {
-  const uiCtx = useContext(UIContext);
-  const [showIcon, setShowIcon] = useState(false);
-  const { width: vw } = useWindowDimensions();
-
-  useEffect(() => {
-    if (vw <= 1023) {
-      uiCtx.setMobile();
-    } else {
-      uiCtx.setNotMobile();
-    }
-  }, [vw]);
-
-  const onScroll = () => {
-    if (80 < window.scrollY) {
-      setShowIcon(true);
-    } else {
-      setShowIcon(false);
-    }
-  };
-
-  useLayoutEffect(() => {
-    window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener('scroll', onScroll);
-  }, []);
-
-  const scrollToTop = () => {
-    scroll.scrollToTop({ duration: 100 });
-  };
-
-  return (
-    <React.Fragment>
-      <header>
-        {!uiCtx.mobile && <Navbar />}
-        {uiCtx.mobile && <HamburgerMenu />}
-      </header>
-      <main>
-        <About />
-        <Skills />
-        <Projects />
-        <AiOutlineToTop
-          className={`to-top-icon ${showIcon ? 'showIcon' : ''}`}
-          size='50px'
-          onClick={scrollToTop}
-        />
-      </main>
-      <Footer />
-    </React.Fragment>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/UI/SectionTitle/SectionTitle.js
+++
@@ -1,8 +0,0 @@
-import classes from './SectionTitle.module.css';
-import React from 'react';
-
-const SectionTitle = (props) => {
-  return <h2 className={classes.h2}>{props.children}</h2>;
-};
-
-export default SectionTitle;
